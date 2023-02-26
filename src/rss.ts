8c11e52f555ba0d315dc827434ffbce0ec6005cf
Dylan Freedman <freedmand@gmail.com>
2023-02-26 01:22:10 -0500

    feat: image links

--- src/rss.ts
+++ src/rss.ts
@@ -23,10 +23,46 @@ export async function fetchRssFeed(url: string): Promise<Rss> {
 		throw new Error('Feed is null');
 	}
 
-	console.log(feed.items);
+	const getImage = (item: any) => {
+		const image: string | undefined = item.media?.[0]?.url;
+		return image;
+	};
+
+	const linksWithoutImages = feed.items
+		.filter((item: any) => !getImage(item))
+		.map((item: any) => item.link);
+
+	// Batch by 10
+	const batches = linksWithoutImages.reduce((acc: string[][], link: string) => {
+		const last = acc[acc.length - 1];
+		if (last == null || last.length === 10) {
+			acc.push([link]);
+		} else {
+			last.push(link);
+		}
+		return acc;
+	}, []);
+
+	let images = await Promise.all(
+		batches.map(async (batch) => {
+			const linksWithImages = await fetch(
+				`${proxy}?images=${encodeURIComponent(JSON.stringify(batch))}`
+			);
+			const images = await linksWithImages.json();
+			return images;
+		})
+	);
+	images = images.flat();
+	const imageMap: { [key: string]: string } = {};
+	images.forEach((image: string, index: number) => {
+		imageMap[linksWithoutImages[index]] = image;
+	});
 
 	return feed.items.map((item: any) => {
-		const image: string | undefined = item.media?.[0]?.url;
+		let image: string | undefined = getImage(item);
+		if (image == null) {
+			image = imageMap[item.link];
+		}
 		return {
 			title: decode(item.title ?? ''),
 			description: decode(item.description ?? ''),
